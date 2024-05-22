52388a55a8deb9d2e98a6f76de842dc74852c376
Asosa17 <alejandrosf913@gmail.com>
2024-05-23 00:49:39 +0200

    V6(homev2, grafico)

--- app/src/main/java/net/azarquiel/appgym/ui/HomeFragment.kt
+++ app/src/main/java/net/azarquiel/appgym/ui/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.echo.holographlibrary.Line
 import com.echo.holographlibrary.LinePoint
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
@@ -20,6 +22,8 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -30,9 +34,13 @@ import com.squareup.picasso.Picasso
 import net.azarquiel.appgym.R
 import net.azarquiel.appgym.databinding.FragmentEjerciciosBinding
 import net.azarquiel.appgym.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnChartValueSelectedListener {
 
+    private lateinit var lineChart: LineChart
     private lateinit var binding: FragmentHomeBinding
     private lateinit var root: View
     private var userlocal: FirebaseUser? = null
@@ -56,59 +64,112 @@ class HomeFragment : Fragment() {
         val username = datosUserSH.getString("username",null)
         Picasso.get().load(imageUrl).into(binding.ivhomeuser)
         binding.tvhomeuser.setText("Hola ${username}")
+        lineChart = binding.barChart
 
-        var lineChart=binding.barChart
+        obtenePesos()
 
-        // Datos para el gráfico
-        val entries = listOf(
-            Entry(.5f, 0f),
-            Entry(1f, 20f),
-            Entry(2f, 15f),
-            Entry(3f, 25f),
-            Entry(4f, 30f),
-            Entry(5f, 30f),
-            Entry(6f, 70f),
-            Entry(7f, 30f),
-            Entry(8f, 30f),
-            Entry(9f, 30f),
-            Entry(10f, 30f),
-            Entry(11f, 18f),
-            Entry(12f, 18f),
-            Entry(12.5f,0f)
-        )
+        lineChart.setOnChartValueSelectedListener(this)
+    }
+
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        e?.let {
+            // Acción al seleccionar un valor
+            Toast.makeText(context, "Valor seleccionado: ${it.y} en el mes: ${it.x.toInt()}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onNothingSelected() {
+        // Acción al deseleccionar un valor
+    }
+    private fun obtenePesos() {
+        val currentUser = auth.currentUser
+        currentUser?.let { user ->
+            val userEmail = user.email
+            userEmail?.let { email ->
+                val pesosdb = db.collection("users").document(email)
+                pesosdb.addSnapshotListener { snapshot, e ->
+                        if (e != null) {
+                            Log.w("TAG", "Listen failed.", e)
+                            return@addSnapshotListener
+                        }
+                        if (snapshot != null && snapshot.exists()) {
+                            val weightsMap = snapshot.data?.get("peso") as Map<String, Any>
+                            val monthlyAverages = calculateMonthlyAverages(weightsMap)
+                            setupChart(monthlyAverages)
+                        }
+                    }
+            }
+        }
+    }
+    private fun calculateMonthlyAverages(weightsMap: Map<String, Any>): List<Entry> {
+        val monthWeights = mutableMapOf<String, MutableList<Double>>()
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val monthFormat = SimpleDateFormat("yyyy-MM", Locale.getDefault())
+
+        for ((dateString, weightString) in weightsMap) {
+            val weight = weightString.toString().toDouble()
+            val date = dateFormat.parse(dateString)
+            val month = monthFormat.format(date)
+            if (monthWeights.containsKey(month)) {
+                monthWeights[month]!!.add(weight)
+            } else {
+                monthWeights[month] = mutableListOf(weight)
+            }
+        }
+
+        val monthlyAverages = ArrayList<Entry>()
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+
+        for (month in 0..11) {
+            calendar.set(Calendar.MONTH, month)
+            val monthString = monthFormat.format(calendar.time)
+            val weights = monthWeights[monthString]
+            val averageWeight = weights?.let { it.sum() / it.size } ?: 0.0
+            monthlyAverages.add(Entry((month + 1).toFloat(), averageWeight.toFloat()))
+        }
+
+        return monthlyAverages
+    }
+
+    private fun setupChart(monthlyAverages: List<Entry>) {
+
+
+        // Agregar puntos ficticios para crear espacio
+        val adjustedEntries = mutableListOf<Entry>()
+        adjustedEntries.add(Entry(0.5f, 0f)) // Punto ficticio al inicio
+        adjustedEntries.addAll(monthlyAverages)
+        adjustedEntries.add(Entry(12.5f, 0f)) // Punto ficticio al final
 
         // Configuración del conjunto de datos
-        val dataSet = LineDataSet(entries, "Datos de ejemplo")
+        val dataSet = LineDataSet(adjustedEntries, "Peso promedio mensual")
         dataSet.color = Color.WHITE
         dataSet.circleColors = listOf(Color.BLUE) // Color de los puntos
         dataSet.valueTextColor = Color.WHITE
+        dataSet.setDrawFilled(false)
         dataSet.fillColor = Color.WHITE
         lineChart.legend.isEnabled = false
 
-        /// Configuración del eje X (Meses)
+        // Configuración del eje X (Meses)
         val xAxis: XAxis = lineChart.xAxis
         xAxis.position = XAxis.XAxisPosition.BOTTOM
         xAxis.textColor = Color.WHITE
         xAxis.setDrawAxisLine(true)
-        xAxis.axisLineWidth= 3f
         xAxis.setDrawGridLines(false)
         xAxis.granularity = 1f // Saltos entre cada etiqueta de mes
         xAxis.valueFormatter = IndexAxisValueFormatter(
-            listOf("","E", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D")
+            listOf("","E", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D","")
         )
 
-        // Configuración del eje Y (Pesos en kg de 20 en 20 hasta 140)
+        val maxWeight = monthlyAverages.maxOfOrNull { it.y } ?: 0f
+        // Configuración del eje Y (Pesos en kg)
         val yAxisRight: YAxis = lineChart.axisRight
         yAxisRight.isEnabled = false
         val yAxisLeft: YAxis = lineChart.axisLeft
         yAxisLeft.textColor = Color.WHITE
         yAxisLeft.axisMinimum = 0f
-        yAxisLeft.axisMaximum = 140f
+        yAxisLeft.axisMaximum = (maxWeight + 20).coerceAtLeast(100f)
         yAxisLeft.granularity = 20f
-        yAxisLeft.gridColor = Color.LTGRAY
-        yAxisLeft.gridLineWidth = .1f // Cambiar el grosor de las líneas de cuadrícula
-        yAxisLeft.axisLineWidth= 3f
-        yAxisLeft.enableGridDashedLine(10f,10f,1f)
 
         // Configuración del gráfico
         val lineData = LineData(dataSet)
@@ -116,21 +177,18 @@ class HomeFragment : Fragment() {
         lineChart.description.isEnabled = false
         lineChart.setNoDataText("No hay datos disponibles")
 
-
-
-        lineChart.isDragXEnabled= true
-        lineChart.isDragYEnabled= false
-        lineChart.setPinchZoom(false)
+        // Configuración del comportamiento del gráfico
+        lineChart.isDragEnabled = true // Habilitar desplazamiento
         lineChart.isScaleXEnabled = false // Deshabilitar zoom en eje X
         lineChart.isScaleYEnabled = false // Deshabilitar zoom en eje Y
+        lineChart.setPinchZoom(false) // Deshabilitar zoom mediante pellizco
         lineChart.isDoubleTapToZoomEnabled = false // Deshabilitar zoom al doble toque
 
-        // Permitir desplazamiento horizontal
-        lineChart.setVisibleXRangeMaximum(3f) // Ajustar el rango visible máximo en el eje X
+        // Permitir desplazamiento horizontal y establecer el rango visible a 3 meses
+        lineChart.setVisibleXRangeMaximum(3f)
         lineChart.moveViewToX(0f)
 
-        lineChart.invalidate()
+        lineChart.invalidate() // Refrescar el gráfico
     }
-
 
 }
